23e68989d78e5c87f58de5f5b9fe47d1e5c276f6
Tasnimmou11 <ummemou44@gmail.com>
2025-03-14 06:50:35 +0600

    nav responsive

--- src/components/layout/Navigation.jsx
+++ src/components/layout/Navigation.jsx
@@ -7,71 +7,81 @@ import { IoMdHeartEmpty } from "react-icons/io";
 import { LuUser } from "react-icons/lu";
 import { IoChevronDown } from "react-icons/io5";
 import { IoGridOutline } from "react-icons/io5";
+import { FaBars } from "react-icons/fa6";
 import { Link } from "react-router";
+
 
 const Navigation = () => {
   const [show, setShow] = useState(false);
   return (
-    <header>
+    <header className="">
       {/* header top part */}
-      <div className="container">
-        <div className="flex justify-between items-center py-8">
-          <Link to="/">
-            <img src="/logo.png" alt="logo" />
+     <div className="shadow-md md:shadow-none ">
+        
+     <div className="container ">
+        <div className="  flex justify-between items-center py-8 gap-4">
+          <button className="md:hidden text-2xl text-primary">
+          <FaBars />
+          </button>
+          <Link to="/" className="w-32 lg:w-auto">
+            <img src="/logo.png" alt="logo" className="w-full" />
           </Link>
 
-          <div className="border-2 border-brand p-5 rounded w-full max-w-[700px] flex items-center">
-            <select name="" id="" className="pr-3.5 border-r-2">
+          <div className="border-2 border-brand p-2 lg:p-5 rounded w-full max-w-64 lg:max-w-[700px] hidden  md:flex items-center ">
+            <select name="" id="" className="pr-3.5 border-r-2 text-primary hidden lg:block">
               <option value="">All Categories</option>
             </select>
             <input
               type="text"
-              placeholder="Search for item"
-              className="px-3.5 w-2/3"
+              placeholder="Search for items"
+              className="px-3.5 w-full lg:w-2/3 outline-0"
             />
             <IoSearchOutline className="ml-auto" />
           </div>
           <div>
-            <ul className="flex gap-5">
+            <ul className=" flex gap-3 :gap-5 text-sm">
               <li>
                 <Link to="/" className="flex gap-2 items-end relative">
-                  <span className="w-6 h-6 bg-brand rounded-full text-white flex item-center justify-center absolute -top-2 right-15">
+                  <span className="w-4 h-4 lg:w-6 lg:h-6 text-xs md:text-sm bg-brand rounded-full text-white flex items-center justify-center absolute -top-2 -right-2 md:-top-2 md:right-15">
                     0
                   </span>
-                  <IoGitCompareOutline className="text-3xl text-secondary" />
-                  Compare
+                  <IoGitCompareOutline className=" text-xl lg:text-3xl text-secondary" />
+                  <span className="hidden md:block">  Compare</span>
                 </Link>
               </li>
               <li>
                 <Link to="/" className="flex gap-2 items-end relative">
-                  <span className="w-6 h-6 bg-brand rounded-full text-white flex item-center justify-center absolute -top-2 right-13">
+                  <span className="w-4 h-4 lg:w-6 lg:h-6 text-xs md:text-sm bg-brand rounded-full text-white flex items-center justify-center absolute -top-2 -right-2 md:-top-2 md:right-11">
                     0
                   </span>
-                  <IoMdHeartEmpty className="text-3xl text-secondary" />
-                  Wishlist
+                  <IoMdHeartEmpty className="text-xl lg:text-3xl text-secondary" />
+                  <span className="hidden md:block">Wishlist</span>
                 </Link>
               </li>
               <li>
                 <Link to="/" className="flex gap-2 items-end relative">
-                  <span className="w-6 h-6 bg-brand rounded-full text-white flex item-center justify-center absolute -top-2 right-6">
+                  <span className="w-4 h-4 lg:w-6 lg:h-6 text-xs md:text-sm bg-brand rounded-full text-white flex items-center justify-center absolute -top-2 -right-2 md:-top-2 md:right-6">
                     0
                   </span>
-                  <GrCart className="text-3xl text-secondary" />
-                  Cart
+                  <GrCart className="text-xl lg:text-3xl text-secondary" />
+                  <span className="hidden md:block">Cart</span>
                 </Link>
               </li>
               <li>
                 <Link to="/" className="flex gap-2 items-end relative">
-                  <LuUser className="text-3xl text-secondary" />
-                  Account
+                  <LuUser className="text-xl lg:text-3xl text-secondary" />
+                  <span className="hidden md:block">Account</span>
                 </Link>
               </li>
             </ul>
           </div>
+          {/* main menu mobile device */}
         </div>
       </div>
+     
+     </div>
       {/* header nav part */}
-      <nav className="border-t border-[#ECECEC] border-b">
+      <nav className="hidden md:block md:border-t md:border-[#ECECEC] md:border-b">
         <div className="container flex items-center gap-9 ">
           <div className="relative">
             <h3
@@ -81,7 +91,7 @@ const Navigation = () => {
               <IoGridOutline /> Browse All Categories <IoChevronDown />
             </h3>
             {show && (
-              <div className="w-md absolute top-22 left-0 p-7 border rounded-md border-brand grid gap-4 grid-cols-2 ">
+              <div className="w-md absolute top-22 left-0 p-7 border rounded-md bg-white border-brand grid gap-4 grid-cols-2 ">
                 <div className="flex items-center gap-2  cursor-pointer rounded-sm py-2 px-4 hover:shadow-md hover:border-brand border border-[#ECECEC]">
                   <img src="/dairies.svg" alt="dairies" className="w-6" />
                   <p className="font-bold text-sm hover:text-brand  text-primary">
@@ -137,7 +147,7 @@ const Navigation = () => {
                 <div className="flex items-center gap-2  cursor-pointer rounded-sm py-2 px-4 hover:shadow-md hover:border-brand border border-[#ECECEC]">
                   <img src="/fruits.svg" alt="fruits" className="w-6" />
                   <p className="font-bold text-sm hover:text-brand  text-primary">
-                    Fresh Fruits{" "}
+                    Fresh Fruits
                   </p>
                 </div>
                 <div className="flex items-center gap-2  cursor-pointer rounded-sm py-2 px-4 hover:shadow-md hover:border-brand border border-[#ECECEC]">
